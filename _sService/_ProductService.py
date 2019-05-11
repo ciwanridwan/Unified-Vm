c8ff6613a31222314906904ceecf60c4680fb34e
wahyudi <yudi@mdd.co.id>
2019-05-11 11:43:45 +0700

    (x) Add DB Improper Removal, (x) Fix Product Update Change, (x) etc.

--- _sService/_ProductService.py
+++ _sService/_ProductService.py
@@ -35,21 +35,36 @@ def change_product_stock(port, stock):
         operator = 'OPERATOR'
         if _UserService.USER is not None:
             operator = _UserService.USER['first_name']
+        _stid = check_product[0]['stid']
         _param = {
             'stock': stock,
-            'stid': check_product[0]['stid'],
+            'stid': _stid,
             'user': operator
         }
+        # Record Local Change
         LAST_UPDATED_STOCK.append(check_product[0])
+        # Log Change To Local And Send Signal Into View
+        _DAO.custom_update(' UPDATE ProductStock SET stock=' + stock + ' WHERE stid="'+_stid+'" ')
+        _KioskService.kiosk_get_product_stock()
         status, response = _NetworkAccess.post_to_url(url=BACKEND_URL + 'change/product-stock', param=_param)
         LOGGER.info(('change_product_stock', str(_param), str(status), str(response)))
         if status == 200 and response['result'] == 'OK':
-            _DAO.custom_query(' UPDATE ProductStock SET stock=' + stock + ' WHERE stid="'+port+'" ')
-            # To Bypass Refresh Product Stock Locally
-            _KioskService.get_product_stock()
+            # _KioskService.kiosk_get_product_stock()
             PR_SIGNDLER.SIGNAL_CHANGE_STOCK.emit('CHANGE_PRODUCT_STOCK|SUCCESS')
         else:
             PR_SIGNDLER.SIGNAL_CHANGE_STOCK.emit('CHANGE_PRODUCT_STOCK|ERROR')
     except Exception as e:
         LOGGER.warning(('change_product_stock', e))
         PR_SIGNDLER.SIGNAL_CHANGE_STOCK.emit('CHANGE_PRODUCT_STOCK|ERROR')
+
+
+def kiosk_get_product_stock():
+    _url = BACKEND_URL + 'get/product-stock'
+    if _Tools.is_online(source='start_get_product_stock') is True:
+        s, r = _NetworkAccess.get_from_url(url=_url)
+        if s == 200 and r['result'] == 'OK':
+            products = r['data']
+            _DAO.flush_table('ProductStock')
+            for product in products:
+                _DAO.insert_product_stock(product)
+            _KioskService.get_product_stock()
